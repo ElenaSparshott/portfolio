5ae5eff9bb7ccc162505c443e94a308045cd5338
Elena Sparshott <65815679+ElenaSparshott@users.noreply.github.com>
2022-04-24 21:49:52 +0100

    tailwind

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,10 +4,35 @@ import Favicon from "../assets/favicon.ico"
 
 const Navbar = () => {
   return (
-    <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a129f] text-gray-300'>
+    <div className='fixed w-full flex justify-between items-center px-4 bg-[#0a129f] text-gray-300'>
         <div>
-            <img src={Favicon} alt="Logo"/>
+            <img src={Favicon} alt="My Logo" style={{width: '50px'}}/>
         </div>
+        <div>
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Skills</li>
+                <li>Projects</li>
+                <li>Contact</li>
+            </ul>
+        </div>
+        {/* {hamburger} */}
+        <div className='hidden'>
+            <FaBars/>
+        </div>
+
+        {/* {mobile menu} */}
+        <ul className='hidden'>
+            <li>Home</li>
+            <li>About</li>
+            <li>Skills</li>
+            <li>Projects</li>
+            <li>Contact</li>
+        </ul>
+
+        {/* {Social icons} */}
+        <div></div>
 
     </div>
   )
