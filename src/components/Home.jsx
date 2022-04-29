51bdbe2f6b0ef6a0aebe34e4fd8dcd186364a8c8
Elena Sparshott <65815679+ElenaSparshott@users.noreply.github.com>
2022-04-29 12:25:07 +0100

    home component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,12 +6,18 @@ const Home = () => {
     <div name='home' className=' w-full h-screen bg-[#9193c3]'>
 
         {/* {container} */}
-        <div className='max-w-[100px] mx-auto px-8 flex flex-col justify-center h-full'>
+        <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
             <p className='text-[#0a819f]'>Hello, my name is</p>
-            <h1>Elena Sparshott</h1>
-            <p>I'm a full stack delevoper working on responsive full stack web applications</p>
+            <h1 className='text-4xl sm:text-7xl font-bold text-[#b7bccd]'>Elena Sparshott</h1>
+            <h2 className='text-4xl sm:text-7xl font-bold text-[#474b59]'>I'm a full stack delevoper.</h2>
+            <p className='text-[#474b59] py-4 max-w[600px]'>I am a full-stack developer working on responsive full stack web applications.</p>
+
             <div>
-                <button>View work <HiArrowNarrowRight /></button>
+                <button className='text-[white] group border-2 px-6 my-2 py-3 flex items-center hover:bg-[#0a819f] hover: border[#0a819f]'>View work 
+                    <span className='group-hover:rotate-90 duration-300'>
+                        <HiArrowNarrowRight className='ml-4'/>
+                    </span>
+                </button>
             </div>
         </div>
         
