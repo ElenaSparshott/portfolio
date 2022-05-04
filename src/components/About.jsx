fe0405981f494017e7aeb074b1856616c49efb61
Elena Sparshott <65815679+ElenaSparshott@users.noreply.github.com>
2022-05-04 19:10:26 +0100

    about information

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,12 +2,15 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div name ="about" className='w-full h-screen bg-[0a192f] text-gray-300'>
+  <div name ="about" className='w-full h-screen bg-[#9193c3] text-[#474b59]'>
         <div className='flex flex-col justify-center items-center w-full h-full'>
             <div className='max-w-[1000px] w-full px-4 grid grid-cols-2 gap-8'>
                     <div className='sm:text-right pb-8 pl-4'>
-                        <p>About</p>
+                        <p className='text-4xl font-bold inline border-b-4 text-[#0a819f]'>About</p>
                     </div>
+                </div>
+                <div className='max-w-[1000px] w-full px-4 grid grid-cols-2 gap-8'>
+                  <p>Hi, I'm Ellie</p>
                 </div>
                 <div>
             </div>
@@ -17,4 +20,4 @@ const About = () => {
   )
 }
 
-export default About
+export default About;
