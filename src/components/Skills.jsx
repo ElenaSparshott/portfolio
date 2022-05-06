78721402e37cec7d370449fc1e769433e098aa31
Elena Sparshott <65815679+ElenaSparshott@users.noreply.github.com>
2022-05-06 16:15:02 +0100

    images

---
+++ src/components/Skills.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import CSS from '../assets/css.png';
+import GITHUB from '../assets/github.png';
+import HTML from '../assets/html.png';
+import JAVASCRIPT from '../assets/javascript.png';
+import NODE from '../assets/node.png';
+import REACT from '../assets/react.png';
+
+export const Skills = () => {
+  return (
+    <div>Skills</div>
+  )
+}
+
+export default Skills
