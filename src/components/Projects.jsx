72512020110082a264674b086fb69df43aee01b8
Elena Sparshott <65815679+ElenaSparshott@users.noreply.github.com>
2022-05-08 22:01:11 +0100

    projects

---
+++ src/components/Projects.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+
+const Projects = () => {
+  return (
+    <div className='w-full text-[#474b59] bg-[#9193c3] md:h-screen '>
+        <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
+            <div className='pb-8'>
+                <p className='text-4xl font-bold inline border-b-4 text-[#474b59] border-[#0a819f]'>Projects</p>
+                <p className='py-6'>Some of my recent projects</p>
+            </div>
+        
+            {/* {set the grid} */}
+            <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
+                {/* card item */}
+                <div className='shasow-lg shadow-[#065a6f] group container rounded-md flex justify-center items-end mx-auto content-div'>
+
+                    {/* { Hover Effect} */}
+                    <div>
+                        <span>
+
+                        </span>
+                        <div>
+                            <a href="/">
+                                <button></button>
+                            </a>
+                            <a href="/">
+                                <button></button>
+                            </a>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+        </div>
+    </div>
+  )
+}
+
+export default Projects
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -13,9 +13,9 @@ export const Skills = () => {
       {/* {container} */}
       <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
         <div>
-          <p className='text-4xl font-bold inline border-b-4 border-[#0a819f]'>Experience</p>
+          <p className='text-4xl text-[#474b59] font-bold inline border-b-4 border-[#0a819f]'>Skills</p>
 
-          <p className='py-6'>These are the technologies I have worked with</p>
+          <p className='py-6 text-[#474b59]'>These are the technologies I have worked with</p>
         </div>
 
 
