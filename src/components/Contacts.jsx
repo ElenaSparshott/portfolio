0e043588ab14ce63c1a97dd8c850e0f9587143dd
Elena Sparshott <65815679+ElenaSparshott@users.noreply.github.com>
2022-05-13 08:23:10 +0100

    getio

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Contacts() {
   return (
     <div name='contact' className='w-full h-screen bg-[#9193c3] flex justify-center items-center p-4'>
-        <form action="" className='flex flex-col max-w-[600px] w-full'>
+        <form method="POST" çaction="https://getform.io/f/f293bd4d-e593-4a14-9d0b-4034dd5a4b01" className='flex flex-col max-w-[600px] w-full'>
             <div className='pb-8'>
                 <p className='text-4xl font-bold inline border-b-4 border-[#0a819f] text-[#474b59]'>Contact</p>
                 <p className='py-4 text-[#474b59]'>Use the form below to send me an email</p>
