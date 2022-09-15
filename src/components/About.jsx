84d14d42f63f155aff3190b9625f7f3319f0767d
Elena Sparshott <65815679+ElenaSparshott@users.noreply.github.com>
2022-09-15 20:46:10 +0100

    new cv

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import ELLIE from '../assets/ellie.jpg'
 
 const About = () => {
   return (
@@ -16,13 +17,14 @@ const About = () => {
                 <div className='max-w-[1000px] w-full px-4 grid grid-cols-2 gap-8'>
                 <div className='sm:text-left text-6xl font-bold'>               
                   <p>Hi, I'm Ellie</p>
-                </div>                  
-                <div className='sm:text-left text-2xl font-bold'>
+                </div>                                
+                <div className='sm:text-left text-3xl font-bold'>
                 <br></br>
                 <br></br>
                   <h1>I am an alumni of the School of Code.
                   I focus on JavaScript building full-stack applications. I have projects written in javaScript and react, dart with flutter and python with flask. The bootcamp taught us problem solving techniques and we worked in teams with agile workflows to build software application.</h1>
                 </div>
+                
             </div>
         </div>
     </div>
